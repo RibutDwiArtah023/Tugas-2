55dccfa3b95aee5541a93c63066795a33a670733
ributdwi01@gmail.com <ributdwi01@gmail.com>
2022-03-13 11:37:07 +0700

    memasukkan file variable.php

---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php
+$a=7;
+$b=5;
+$c=9;
+$d=15;
+$e=8;
+$f=11;
+$penjumlahan= $a+$b+$e;
+$pengurangan= $f-$a;
+$perkalian= $c*$d;
+echo"hasil dari penjumlahan adalah $penjumlahan </br>";
+echo"hasil dari pengurangan adalah $pengurangan </br>";
+echo"hasil dari perkalian adalah $perkalian </br>";
+?>
